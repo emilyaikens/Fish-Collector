87491544bb33f0e013e2ae8a2a750f3e1663f287
emilyaikens <emily.c.aikens@gmail.com>
2022-09-13 14:00:55 -0700

    CRUD operations operational. Edit and delete are good to go"

--- fishcollector/main_app/urls.py
+++ fishcollector/main_app/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     #http://localhost:8000/fish
     path('fish/', views.fish_index, name='index'),
     #http://localhost:8000/fish/1
-    path('fish/<int:fish_id>/', views.fish_detail, name='detail')
+    path('fish/<int:fish_id>/', views.fish_detail, name='detail'),
+    path('fish/create/', views.FishCreate.as_view(), name='fish_create'),
+    path('fish/<int:pk>/update/', views.FishUpdate.as_view(), name='fish_update'),
+    path('fish/<int:pk>/>delete/', views.FishDelete.as_view(), name='fish_delete'),
 ]
--- fishcollector/main_app/views.py
+++ fishcollector/main_app/views.py
@@ -1,3 +1,6 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Fish
@@ -17,3 +20,22 @@ def fish_index(request):
 def fish_detail(request, fish_id):
     fish = Fish.objects.get(id=fish_id)
     return render(request, 'fish/detail.html', {'fish':fish})
+
+class FishCreate(CreateView):
+    model = Fish
+    fields = '__all__'
+    # fields = ['age', 'description'] - use this if you want to specify what the form asks for
+
+    def get_success_url(self, **kwargs):
+        return reverse('detail', args=(self.object.id, ))
+
+class FishUpdate(UpdateView):
+    model = Fish
+    fields = '__all__'
+
+    def get_success_url(self, **kwargs):
+        return reverse('detail', args=(self.object.id, ))
+
+class FishDelete(DeleteView):
+    model = Fish
+    success_url = '/fish/'
