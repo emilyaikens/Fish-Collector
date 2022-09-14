6bd646932b7fb9e5849dcecfffede82c6fbc56ed
emilyaikens <emily.c.aikens@gmail.com>
2022-09-14 14:12:39 -0700

    update admin.py with new Surveys class

--- fishcollector/main_app/admin.py
+++ fishcollector/main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Fish
+from .models import Fish, Surveys
 
 # Register your models here.
 admin.site.register(Fish)
+admin.site.register(Surveys)
