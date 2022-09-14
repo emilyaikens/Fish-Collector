c1fc2c4bbaccd2fa84d91edc23089b3af99bcbbe
emilyaikens <emily.c.aikens@gmail.com>
2022-09-14 14:11:06 -0700

    add AGE class to models

--- fishcollector/main_app/models.py
+++ fishcollector/main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-# Create your models here.
+#used for dropdown menu
+AGE = (
+    ('L', 'Larval'),
+    ('J', 'Juvenile'),
+    ('S', 'Subadult'),
+    ('A', 'Adult')
+)
 
 class Fish(models.Model):
     commonName = models.CharField(max_length=100)
@@ -11,3 +17,19 @@ class Fish(models.Model):
 #allows us to see the cat name in the admin, rather than CatObject1
     def __str__(self):
         return self.commonName
+
+class Surveys(models.Model):
+    date = models.DateField() 
+    age = models.CharField(
+        max_length=1,
+        choices=AGE, #add the choices field option
+        default=AGE[3][0] #set default value to A
+    )
+
+    fish = models.ForeignKey(Fish, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_age_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
