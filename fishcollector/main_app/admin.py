acf05225d7160567e463c1fa94388ea38e7ece3c
emilyaikens <emily.c.aikens@gmail.com>
2022-09-15 18:00:14 -0700

    import and register Collector in admin.py

--- fishcollector/main_app/admin.py
+++ fishcollector/main_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Fish, Surveys
+from .models import Fish, Surveys, Collector
 
 # Register your models here.
 admin.site.register(Fish)
 admin.site.register(Surveys)
+admin.site.register(Collector)
+
 
 ##register Toy equivalent here, and add to import at top (line 2)
